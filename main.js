0abc7ffa7fd15353e52aade2f25fd851d129f0a3
Srushtika <n.srushtika@gmail.com>
2018-10-09 17:10:20 +0100

    Efficiency improved in multiple messages to be marked seen logic

--- main.js
+++ main.js
@@ -3,52 +3,51 @@ function getRandomArbitrary(min, max) {
 }
 
 var avatarsInAssets = [
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_8.png?1536042504672',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_3.png?1536042507202',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_6.png?1536042508902',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_10.png?1536042509036',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_7.png?1536042509659',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_9.png?1536042513205',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_2.png?1536042514285',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_1.png?1536042516362',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_4.png?1536042516573',
-  'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_5.png?1536042517889'
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_8.png?1536042504672',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_3.png?1536042507202',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_6.png?1536042508902',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_10.png?1536042509036',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_7.png?1536042509659',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_9.png?1536042513205',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_2.png?1536042514285',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_1.png?1536042516362',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_4.png?1536042516573',
+    'https://cdn.glitch.com/0bff6817-d500-425d-953c-6424d752d171%2Favatar_5.png?1536042517889'
 ]
 
-var myAvatar = avatarsInAssets[getRandomArbitrary(0, 9)]
-var otherAvatar
-var me = {};
-me.avatar = myAvatar
+var otherAvatar;
+var my = {};
+my.avatar = avatarsInAssets[getRandomArbitrary(0, 9)];
 var you = {};
-var msgObjArray = [];
+var myMsgArray = [];
 
 //----------------------------AblyStuff
-var myId = "id-" + Math.random().toString(36).substr(2, 16)
-var apiKey = '<ABLY-API-KEY>'
+var myId = "id-" + Math.random().toString(36).substr(2, 16);
+var apiKey = 'Aq4Xig.PPFgdg:2Q2Ekgj-a4nUGkxm';
 var ably = new Ably.Realtime({
     key: apiKey,
     clientId: myId,
     echoMessages: false
-})
-var chatChannel = ably.channels.get("chat")
-var readReceiptsChannel = ably.channels.get("read-receipts")
-var presenceChannel = ably.channels.get("presence")
+});
+var chatChannel = ably.channels.get("chat");
+var readReceiptsChannel = ably.channels.get("read-receipts");
+var presenceChannel = ably.channels.get("presence");
 //----------------------------
 
 
 chatChannel.subscribe("userAvatar", (data) => {
-    var dataObj = JSON.parse(JSON.stringify(data))
+    var dataObj = JSON.parse(JSON.stringify(data));
     if (dataObj.clientId != myId) {
-        otherAvatar = dataObj.data.avatar
-        you.avatar = otherAvatar
+        otherAvatar = dataObj.data.avatar;
+        you.avatar = otherAvatar;
     }
-})
+});
 
 presenceChannel.presence.subscribe('enter', function (member) {
     if (member.clientId != myId) {
         chatChannel.publish("userAvatar", {
-            "avatar": myAvatar
-        })
+            "avatar": my.avatar
+        });
     }
 });
 presenceChannel.presence.enter();
@@ -56,8 +55,8 @@ presenceChannel.presence.get(function (err, members) {
     for (var i in members) {
         if (members[i].clientId != myId) {
             chatChannel.publish("userAvatar", {
-                "avatar": myAvatar
-            })
+                "avatar": my.avatar
+            });
         }
     }
 });
@@ -74,21 +73,26 @@ function formatAMPM(date) {
 }
 
 function insertChat(who, text, currentMsgId) {
-    var control = "";
+    var messageBoxHTML = "";
     var date = formatAMPM(new Date());
-    var seenStatus = false
+    var seenStatus = false;
     if (who == "me") {
-        control = '<li class="self">' +
+        messageBoxHTML = '<li class="self">' +
             '<div style="color: #e0e0de" class="avatar">' +
-            '<img src="' + me.avatar + '" />' +
+            '<img src="' + my.avatar + '" />' +
             '/</div>' +
             '<div class="messages">' +
             '<p>' + text + '</p>' +
             '<div><time datetime="2009-11-13T20:00">' + date + '<div style="text-align:right;" id="' + currentMsgId + '"> sent </div>' + '</time>' +
             '</div>' +
             '</li>';
+        var myMsgStatus = {};
+        myMsgStatus["msgId"] = currentMsgId;
+        myMsgStatus["seen"] = false;
+        myMsgStatus["delivered"] = false;
+        myMsgArray.push(myMsgStatus);
     } else {
-        control = '<li class="other">' +
+        messageBoxHTML = '<li class="other">' +
             '<div style="color: #e0e0de" class="avatar">' +
             '<img src="' + you.avatar + '" />' +
             '/</div>' +
@@ -98,31 +102,26 @@ function insertChat(who, text, currentMsgId) {
             '</div>' +
             '</li>';
         if (document.hasFocus()) {
-            for (var i = 0; i < msgObjArray.length; i++) {
-                seenStatus = true
-            }
+            seenStatus = true;
         }
         readReceiptsChannel.publish("receipt", {
             "delivered": true,
             "msgId": currentMsgId,
             "seen": seenStatus,
-            "singleMsgSeen": true
-        })
+            "alreadyActive": true
+        });
     }
-    var seenObj = {}
-    seenObj["msgId"] = currentMsgId
-    seenObj["seen"] = seenStatus
-    msgObjArray.push(seenObj)
-    $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
+
+    $("ul").append(messageBoxHTML).scrollTop($("ul").prop('scrollHeight'));
 
 }
 
 window.onfocus = function () {
     readReceiptsChannel.publish("receipt", {
-        "singleMsgSeen": false,
-        "seen": true
+        "alreadyActive": false,
+        "seen": true,
+        "delivered": true,
     })
-
 };
 
 function resetChat() {
@@ -131,53 +130,66 @@ function resetChat() {
 
 window.onload = inputKeyUpEvent;
 
-function inputKeyUpEvent(){
+function inputKeyUpEvent() {
     var input = document.getElementById("myMsg");
-    input.addEventListener("keyup", function(event) {
-      event.preventDefault();
-      if (event.keyCode === 13) {
-        document.getElementById("sendMessage").click();
-      }
+    input.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("sendMessage").click();
+        }
     });
 }
 
 
 function sendMyMessage() {
-    var currentMsgId = "msg-id-" + Math.random().toString(36).substr(2, 6)
-    var text = document.getElementById("myMsg").value
-    document.getElementById("myMsg").value = ""
+    var newMsgId = "msg-id-" + Math.random().toString(36).substr(2, 6);
+    var text = document.getElementById("myMsg").value;
+    document.getElementById("myMsg").value = "";
     if (text !== "") {
-        insertChat("me", text, currentMsgId);
+        insertChat("me", text, newMsgId);
         chatChannel.publish("chatMessage", {
             message: text,
-            msgId: currentMsgId
-        })
+            localMsgId: newMsgId
+        });
         $(this).val('');
     }
 }
 
 readReceiptsChannel.subscribe("receipt", (data) => {
-    var dataObj = JSON.parse(JSON.stringify(data))
-    if (dataObj.data.singleMsgSeen) {
-        var receiptMsgId = dataObj.data.msgId
-        if (dataObj.data.seen) {
-            if (document.getElementById(receiptMsgId)) {
-                (document.getElementById(receiptMsgId)).innerHTML = 'seen'
+    var dataObj = JSON.parse(JSON.stringify(data));
+    var receiptMsgId = dataObj.data.msgId;
+    if (dataObj.data.seen && dataObj.data.alreadyActive) {
+        myMsgArray.forEach((arrData, index)=>{
+            if (arrData.msgId == receiptMsgId) {
+                if (document.getElementById(receiptMsgId)) {
+                    (document.getElementById(receiptMsgId)).innerHTML = 'seen';
+                }
             }
-        } else if (dataObj.data.delivered) {
-            if (document.getElementById(receiptMsgId)) {
-                (document.getElementById(receiptMsgId)).innerHTML = 'delivered'
+            myMsgArray.splice(index,1)
+        })
+        for (var i = 0; i < myMsgArray.length; i++) {
+            if (myMsgArray.msgId == receiptMsgId) {
+                if (document.getElementById(receiptMsgId)) {
+                    (document.getElementById(receiptMsgId)).innerHTML = 'seen';
+                }
             }
         }
-    } else if (!dataObj.data.singleMsgSeen) {
-        for (var i = 0; i < msgObjArray.length; i++) {
-            var id = msgObjArray[i].msgId
-            if (document.getElementById(id)) {
-                (document.getElementById(id)).innerHTML = 'seen'
-            }
+    } else if (dataObj.data.seen && !dataObj.data.alreadyActive) {
+        while(myMsgArray.length){
+            myMsgArray.forEach((arrData, index)=> {
+                if(!arrData.seen){
+                    if (document.getElementById(arrData.msgId)) {
+                        (document.getElementById(arrData.msgId)).innerHTML = 'seen';
+                    }
+                    myMsgArray.splice(index,1)
+                }
+            })
+        }
+    } else if (dataObj.data.delivered) {
+        if (document.getElementById(receiptMsgId)) {
+            (document.getElementById(receiptMsgId)).innerHTML = 'delivered';
         }
     }
-
 })
 
 //-- Clear Chat
@@ -185,8 +197,23 @@ resetChat();
 
 //-- Print Messages
 chatChannel.subscribe("chatMessage", (data) => {
-    var dataObj = JSON.parse(JSON.stringify(data))
-    var message = dataObj.data.message
-    var myMsgId = dataObj.data.msgId
-    insertChat("you", message, myMsgId);
+    var dataObj = JSON.parse(JSON.stringify(data));
+    var message = dataObj.data.message;
+    var receivedMsgId = dataObj.data.localMsgId;
+    insertChat("you", message, receivedMsgId);
+    if (document.hasFocus()) {
+        readReceiptsChannel.publish("receipt", {
+            "delivered": true,
+            "msgId": receivedMsgId,
+            "seen": true,
+            "alreadyActive": true
+        });
+    } else {
+        readReceiptsChannel.publish("receipt", {
+            "delivered": true,
+            "msgId": receivedMsgId,
+            "seen": false,
+            "alreadyActive": false
+        });
+    }
 })
